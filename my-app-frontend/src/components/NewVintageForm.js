6bbac984534225597f678e3fd0a7d41f7edb8265
James <jamesschmidt124@gmail.com>
2021-11-17 15:31:04 -0800

    Added to Buy working on Sell

---
+++ my-app-frontend/src/components/NewVintageForm.js
@@ -0,0 +1,53 @@
+import React, {useState} from "react";
+
+// name, price, product_despcription, imgUrl, user_id, id
+const initialvalue= {
+    name: '',
+    price: '',
+    product_despcription: '',
+    imgUrl: '',
+    user_id: '',
+    
+  }
+  function NewVintageForm({setVintage}) {
+      const [NewVintage, setNewVintage]= useState(initialvalue)
+      function handleChange(e){
+          setNewVintage((currentNewVintage)=>{
+              return{
+                  ...currentNewVintage,
+                  [e.target.name]: e.target.value,
+              }
+          })
+      }
+      function handleSubmit(e){
+        e.preventDefault()  
+        fetch('http://localhost:9292/items',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(NewVintage)
+          }).then(resp=> resp.json())
+            .then((data)=>{
+                setVintage((currentNewVintage)=>[...currentNewVintage, data])
+                setNewVintage(initialvalue)
+            })
+      }
+      return (
+          <main className='new-vintage-form'>
+              <h2>New Vintage</h2>
+              <form className="apple" onSubmit={handleSubmit}>
+                  <input className="name" type='text' name='name' placeholder='Product Name' value={NewVintage.name} onChange={handleChange}/>
+                  <input className="price" type='text' name='price' placeholder='Price' value={NewVintage.price} onChange={handleChange}/>
+                  <input className="product_despcription" name='product despcription' placeholder='Product despcription' value={NewVintage.product_despcription} onChange={handleChange}/>
+                  <input className="imgUrl" type='text' name='Product Image' placeholder='Product Image' value={NewVintage.imgUrl} onChange={handleChange}/>
+                  <input className="user_id" type='text' name='Username' placeholder='Username' value={NewVintage.user_id} onChange={handleChange}/>
+                  <button className="submit" type="submit">Sell Vintage</button>
+                  
+
+              </form>
+          </main>
+        
+      )
+  }
+  export default NewVintageForm
--- my-app-frontend/src/components/VintageCard.js
+++ my-app-frontend/src/components/VintageCard.js
@@ -1,14 +1,22 @@
-
+import {Link} from "react-router-dom";
 import React, {useState} from "react";
 
-function VintageCard({vintage: {name, price, product_despcription, imgUrl, user_id}}){
-    const [showBuy, setShowBuy]=useState(true)
+function VintageCard({vintage: {name, price, product_despcription, imgUrl, user_id, id}}){
+    const [showBuy, setShowBuy]=useState(false)
     function handleClick(){
         setShowBuy((currentShowBuy)=> !currentShowBuy)
     }
     return (
-        <div onClick={handleClick}>
-            {name}
+        <div >
+            
+            <div>{showBuy ? null: name}</div>
+            <div >{showBuy ? price: null}</div>
+            {product_despcription}
+            <img src={imgUrl}/>
+            <Link to= {`/Buy/${id}`}>Buy</Link>
+            
+            {user_id}
+
         </div>
     )
 }
